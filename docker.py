2e77d0cb8349c33a151de042670e6a5cf76d2851
Premchand <Premchandg278@gmail.com>
2020-09-29 22:03:56 +0530

    Create docker.py

---
+++ docker.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+import subprocess
+import cgi
+
+print('content-type: text/html')
+print()
+data = cgi.FieldStorage()
+uname = data.getvalue("uname")
+pwd  = data.getvalue("pwd")
+operation = data.getvalue("operation")
+if  uname == "root" and pwd == "prem" and operation == "docker" :
+    image = data.getvalue("image")
+    name = data.getvalue("name")
+    port1 = data.getvalue("port1")
+    port2 = data.getvalue("port2")
+    localpath =  data.getvalue("localpath")
+    dockerpath = data.getvalue("dockerpath")
+    name = " --name  {}".format(name)
+    if "None" in name :
+        name = ""
+    port = " -p {}:{}".format(port1,port2)
+    if "None" in port :
+        port = ""
+    volume = " -v {}:{}".format(localpath,dockerpath)
+    if "None" in volume :
+        volume = ""
+    cmd  = "sudo docker container run -dit {1} {2} {3} {0} ".format(image,name,port,volume)
+    cmd = cmd.replace('None' , "")
+    output = subprocess.getoutput(cmd)
+    print(cmd)
+    print("<pre>",output,"</pre>")
+elif uname == "root" and pwd == "prem" and operation == "status" :
+    status == data.getvalue("status")
+    print("this code run")
+    print(status)
+    if status == "dockerengine":
+        output = subprocess.getoutput("sudo systemctl status docker")
+    elif status == "container":
+        output == subprocess.getoutput("sudo docker ps -a")
+    elif status == "runningcontainer":
+        output == subprocess.getoutput("sudo docker ps")
+    elif status == "images":
+        output == subprocess.getoutput("sudo docker images")
+    else :
+        output = "invalid"
+    print("<pre>",output,"</pre>")
+elif uname == "root" and pwd == "prem" and operation == "start/stop":
+    containername = data.getvalue("containername")
+    if status == "stop":
+        output = subprocess.getoutput("sudo docker container stop {}".format(containername))
+    elif status == "start":
+        output = subprocess.getoutput("sudo docker container start {}".format(containername))
+    elif status == "remove":
+        output = subprocess.getoutput("sudo docker container rm -f {}".format(containername))
+    elif status == "allremove":
+        output = subprocess.getoutpuy("sudo docker container rm -f $(docker ps -aq)")
+    else:
+        output = "Invalid"
+    print("<pre>",output,"</pre>")
+elif uname == "root" and pwd == "prem" and operation == "cmd" :
+    cmd = data.getvalue("cmd")
+    cmd = "sudo {}".format(cmd)
+    output = subprocess.getoutput(cmd)
+    print("<pre>",output,"</pre>")
+
+elif uname == "root" and pwd == "prem":
+    print("succesfully login")
+else:
+    print("<h1>Authentication Error !!!!!</h1>")
